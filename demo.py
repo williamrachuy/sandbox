6e838a6d4adb9f2f14a6a8db6c525f3f3094f49b
williamrachuy <williamrachuy@gmail.com>
2018-09-11 00:00:01 -0700

    bulk upload

--- demo.py
+++ demo.py
@@ -14,26 +14,27 @@ classes = 10  # digits
 batch = word_batch = speech_data.mfcc_batch_generator(batch_size)
 X, Y = next(batch)
 trainX, trainY = X, Y
-testX, testY = X, Y #overfit for now
+testX, testY = X, Y  # overfit for now
 
 # Network building
 net = tflearn.input_data([None, width, height])
 net = tflearn.lstm(net, 128, dropout=0.8)
 net = tflearn.fully_connected(net, classes, activation='softmax')
-net = tflearn.regression(net, optimizer='adam', learning_rate=learning_rate, loss='categorical_crossentropy')
+net = tflearn.regression(
+    net, optimizer='adam', learning_rate=learning_rate, loss='categorical_crossentropy')
 # Training
 
-### add this "fix" for tensorflow version errors
+# add this "fix" for tensorflow version errors
 col = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
 for x in col:
-	tf.add_to_collection(tf.GraphKeys.VARIABLES, x ) 
+    tf.add_to_collection(tf.GraphKeys.VARIABLES, x)
 
 
 model = tflearn.DNN(net, tensorboard_verbose=0)
-while 1: #training_iters
-  model.fit(trainX, trainY, n_epoch=10, validation_set=(testX, testY), show_metric=True,
-		  batch_size=batch_size)
-  _y=model.predict(X)
+while 1:  # training_iters
+    model.fit(trainX, trainY, n_epoch=10, validation_set=(testX, testY), show_metric=True,
+              batch_size=batch_size)
+    _y = model.predict(X)
 model.save("tflearn.lstm.model")
-print (_y)
-print (y)
+print(_y)
+print(y)
